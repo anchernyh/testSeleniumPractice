0807b0a841017d2f8e81a50d0c9a3883f954769b
Anastasiia Chernykh <anchernykh15@gmail.com>
2022-03-17 17:14:20 +0200

    Add extracted method

--- src/test/java/helper/TestBase.java
+++ src/test/java/helper/TestBase.java
@@ -66,4 +66,22 @@ public class TestBase {
             acceptNextAlert = true;
         }
     }
+
+    protected void fillNameSurnameMail() {
+        driver.get("https://demoqa.com/");
+        String name = "Anastasiia";
+        String surname = "Test";
+        String mail = "name@mail.ua";
+        driver.findElement(By.xpath("//div[@id='app']/div/div/div[2]/div/div[2]/div/div")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Forms'])[2]/following::span[1]")).click();
+        driver.findElement(By.id("firstName")).click();
+        driver.findElement(By.id("firstName")).clear();
+        driver.findElement(By.id("firstName")).sendKeys(name);
+        driver.findElement(By.id("lastName")).click();
+        driver.findElement(By.id("lastName")).clear();
+        driver.findElement(By.id("lastName")).sendKeys(surname);
+        driver.findElement(By.id("userEmail")).click();
+        driver.findElement(By.id("userEmail")).clear();
+        driver.findElement(By.id("userEmail")).sendKeys(mail);
+    }
 }
